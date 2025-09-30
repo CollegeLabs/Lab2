27dc271af5b1ecb574afbaea29ea8036f4eef779
ConnorQR <5050771@nwpolytech.ca>
2025-09-30 14:19:47 -0600

    Final Test?

--- cs3220_2025f/src/locations.py
+++ cs3220_2025f/src/locations.py
@@ -2,7 +2,7 @@
 loc_A, loc_B = (0, 0), (1, 0)
 
 #Task 1 Rooms
-Room1, Room2, Room3, Room4, Room5 = (0, 0)
+Room1, Room2, Room3, Room4, Room5 = (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)
 
 # for the Task3
 loc_C, loc_D =(1, 0), (1, 1)
