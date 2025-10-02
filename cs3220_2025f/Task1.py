f18673e03eedbf95e4ffd3ef32b21e68e93870c4
ConnorQR <5050771@nwpolytech.ca>
2025-10-02 11:31:48 -0600

    Give me back my If statement

---
+++ cs3220_2025f/Task1.py
@@ -0,0 +1,42 @@
+import random
+import collections
+from src.locations import *
+from src.thingClass import Thing
+from src.agentClass import Agent
+from src.agents import *
+from src.CrazyHouseEnvironment import CrazyHouseEnvironment
+from src.Task3YourClasses import Milk, Mouse, Dog
+
+def RandomAgentProgram(actions):
+   return lambda percept: random.choice(actions)
+
+actions = ['Move_Right', 'Move_Left', 'Eat', 'Drink', 'Fight']
+
+House = CrazyHouseEnvironment()
+
+Cat=RandomCatAgent()
+
+Milk=Milk()
+Mouse=Mouse()
+Dog=Dog()
+
+House.add_thing(Milk)
+House.add_thing(Mouse)
+House.add_thing(Dog)
+
+
+while True:
+    if Dog.location == Mouse.location:
+        House.delete_thing(Mouse)
+        House.add_thing(Mouse)
+    if Dog.location != Mouse.location:
+        break
+
+if Milk.location == Mouse.location:
+    House.delete_thing(Milk)
+
+House.add_thing(Cat)
+print("Cat is located at {}.".format(Cat.location))
+
+House.run()
+
